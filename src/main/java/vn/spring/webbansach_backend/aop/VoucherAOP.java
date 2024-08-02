207c046bd758e04f22ebcd5da1b1d8a24bd3ebd8
DangDangHoccOdE <haidang972004@gmail.com>
2024-08-02 16:14:17 +0700

    update voucherAOP

--- src/main/java/vn/spring/webbansach_backend/aop/VoucherAOP.java
+++ src/main/java/vn/spring/webbansach_backend/aop/VoucherAOP.java
@@ -5,6 +5,8 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
+import vn.spring.webbansach_backend.entity.User;
+import vn.spring.webbansach_backend.service.inter.IUserService;
 import vn.spring.webbansach_backend.utils.SecurityUtils;
 
 import java.util.Map;
@@ -22,6 +24,20 @@ public class VoucherAOP {
     @Before("execution(* vn.spring.webbansach_backend.controller.VoucherController.saveVoucherByUser(..)) && args(..,map)")
     public void checkAccess(Map<String,Integer> map) throws AccessDeniedException {
         long userId = map.get("userId");
+
+        if (!securityUtils.hasAccessByUserId(userId)){
+            throw new AccessDeniedException("Bạn không có quyền truy cập!");
+        }
+    }
+
+    @Before("execution(* vn.spring.webbansach_backend.controller.VoucherController.showVoucherByUserId(..)) && args(..,userId)")
+    public void checkAccess(Long userId) throws AccessDeniedException {
+        if (!securityUtils.hasAccessByUserId(userId)){
+            throw new AccessDeniedException("Bạn không có quyền truy cập!");
+        }
+    }
+    @Before(value = "execution(* vn.spring.webbansach_backend.controller.VoucherController.findVoucherByVoucherCodeAndUserId(..)) && args(..,code,userId)", argNames = "code,userId")
+    public void checkAccess(String code,Long userId) throws AccessDeniedException {
 
         if (!securityUtils.hasAccessByUserId(userId)){
             throw new AccessDeniedException("Bạn không có quyền truy cập!");
