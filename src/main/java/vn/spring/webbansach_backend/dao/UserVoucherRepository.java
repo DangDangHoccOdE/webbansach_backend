421f1cc19386013d88b5f7dec0c87eeb4ccc5bd4
DangDangHoccOdE <haidang972004@gmail.com>
2024-08-14 21:56:31 +0700

    update uservoucherRepo

--- src/main/java/vn/spring/webbansach_backend/dao/UserVoucherRepository.java
+++ src/main/java/vn/spring/webbansach_backend/dao/UserVoucherRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.RequestParam;
 import vn.spring.webbansach_backend.entity.UserVoucher;
 import vn.spring.webbansach_backend.entity.UserVoucherId;
-
+import java.util.*;
 @RepositoryRestResource(path = "user-voucher")
 public interface UserVoucherRepository extends JpaRepository<UserVoucher, UserVoucherId> {
     UserVoucher findByVoucher_CodeAndUser_UserId(@RequestParam("code") String code,@RequestParam("userId") Long userId);
+
+    List<UserVoucher> findByUser_UserId(@RequestParam("userId") Long userId);
 }
